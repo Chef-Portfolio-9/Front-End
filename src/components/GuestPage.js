bb0e044e29697491e6b4f445f70ffc40d25f5703
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-06 14:14:26 -0800

    added guestpage.js

---
+++ src/components/GuestPage.js
@@ -0,0 +1,35 @@
+/* jshint esversion: 6 */
+import React, { useState, useEffect } from "react";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+
+
+
+const GuestPage = () => {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+
+
+return(
+
+<Typography variant="h2">Chef Portfolio</Typography>
+
+
+
+
+)
+
+
+
+}
+
+
+
+export default GuestPage;
