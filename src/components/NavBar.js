bfaee999ead873c73504b98654d971a77050c5a5
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-07 23:16:01 -0800

    add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+/* jshint esversion: 6 */
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {AppBar, Toolbar, Typography, Button, IconButton} from '@material-ui/core/';
+import MenuIcon from '@material-ui/icons/Menu';
+import OpenMenu from './Menu.js'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+     textAlign: "left"
+  },
+}));
+
+
+const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <OpenMenu />
+          <Typography variant="h6" className={classes.title}>
+            Chef Portfolio
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default NavBar;
