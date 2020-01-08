55e779564a4c954e8b58e38bcaa9e639cd42d1f6
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-07 17:53:17 -0800

    update recipe and portfolio form

--- src/components/Chef/RecipeForm.js
+++ src/components/Chef/RecipeForm.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 6 */
 
 import React, { useState } from "react";
-import { Button,TextField,Container ,Switch,FormControlLabel ,Typography,TextareaAutosize, Paper } from '@material-ui/core';
+import {Select, FormControl,InputLabel, Button,TextField,Container ,Switch,FormControlLabel ,Typography,TextareaAutosize, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -13,6 +13,9 @@ const useStyles = makeStyles(theme => ({
       width: 250,
     },
   },
+  paper:{
+
+  }
 
 }));
 
@@ -27,6 +30,29 @@ export default function RecipeForm() {
       <h2>Add Recipe</h2>
     <Paper className={classes.paper}>
     <div>
+    <FormControl className={classes.formControl}>
+    <InputLabel htmlFor="age-native-simple">Meal Type</InputLabel>
+    <Select
+      native
+
+      inputProps={{
+        name: 'Meal Type',
+        id: 'meal-type',
+      }}
+    >
+      <option value="" />
+      <option>Breakfast</option>
+      <option>Lunch</option>
+      <option>Dinner</option>
+      <option>Snack</option>
+      <option>Dessert</option>
+    </Select>
+  </FormControl>
+
+
+    </div>
+    <br/>
+    <div>
 
     <TextField
       variant="outlined"
@@ -39,6 +65,7 @@ export default function RecipeForm() {
       autoFocus
 
     />
+
     </div>
       <div>
         <TextField
@@ -58,8 +85,27 @@ export default function RecipeForm() {
           variant="outlined"
         />
       </div>
+      <br/>
+
+<div>
+      <Button  variant="contained" component="label">
+        Upload Recipe Image
+        <input
+          type="file"
+          style={{ display: "none"}}
+        />
+      </Button>
+      </div>
+      <br/>
+
     </Paper>
       <div> <br/> </div>
+
+
+
+
+
+
       <Button type="submit" variant="contained" color="secondary" margin="normal">
         Submit
       </Button>
