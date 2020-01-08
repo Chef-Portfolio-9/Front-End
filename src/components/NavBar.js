a63e4ab46e76d476ebe968a6777b17b20de2f9a1
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-08 09:29:50 -0800

    navbar update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {AppBar, Toolbar, Typography, Button, IconButton} from '@material-ui/core/';
 import MenuIcon from '@material-ui/icons/Menu';
-import OpenMenu from './Menu.js'
+import OpenMenu from './Menu.js';
 
 const useStyles = makeStyles(theme => ({
   root: {
