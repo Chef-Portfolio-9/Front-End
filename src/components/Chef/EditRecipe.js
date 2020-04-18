bfc7c77453217f3540a9ed8de7f926aaceaa40f2
Aaron <aarongillies88@gmail.com>
2020-04-18 16:59:43 -0400

    Fixed edit recipe functionality for chef dashboard

--- src/components/Chef/EditRecipe.js
+++ src/components/Chef/EditRecipe.js
@@ -1,317 +1,122 @@
+import React, { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  Button,
+  TextField,
+  Paper,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import NavBar from "../../components/NavBar.js";
+import { updateRecipe } from "../../actions/RecipeActions/UpdateRecipe";
+import { useLocation } from "react-router-dom";
 
-// import React, { useState, useEffect } from "react";
-// import {Select, FormControl,InputLabel, Button,TextField,Container ,Switch,FormControlLabel, Typography,TextareaAutosize, Paper } from '@material-ui/core';
-// import { makeStyles } from '@material-ui/core/styles';
-// import { connect } from 'react-redux';
-// import NavBar from '../../components/NavBar.js';
-// import { updateRecipe } from '../../actions/RecipeActions/UpdateRecipe';
-// import AxiosWithAuth from "../../utils/AxiosWithAuth.js";
- 
-// const EditRecipe= (props) => {
-//    const id = props.match.params.id 
-//    const userID = localStorage.getItem("userID");
-  //  const [update, setUpdate]= useState({
-  //   recipe_name: '',
-  //   meal_type: '',
-  //   recipe_id: id
-  //  })
-  // const [recipe, setRecipe] = useState({})
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 250,
+    },
+  },
+  paper: {},
+}));
 
-  //  useEffect(() => {
-  //   AxiosWithAuth()
-  //     .get(`/api/recipes/${id}`)
-  //     .then(res => {
-  //       console.log(res)
-  //       setRecipe(res.data)
-  //     })
-  //     .catch(error => {
-  //       console.log('the data was not returned', error)
-  //     })
-  // },[])
+const EditRecipe = (props) => {
+  const classes = useStyles();
+  const userID = localStorage.getItem("userID");
+  let location = useLocation();
 
-  // const updateRecipe = event => {
-  //   event.preventDefault();
-  //   setRecipe(recipe)
-  //   console.log(recipe)
-  //   AxiosWithAuth()
-  //   .put(`/api/recipes/${id}`)
-  //   .then(res => {
-  //     props.history.push('/ChefDashboard')
-  //   });
-  // };
-   
-  // const handleChanges = e => {
-  //   e.preventDefault();
-  //   setRecipe({ ...recipe, [e.target.name]: e.target.value });
-  //  };
-  //  const submitUpdatedRecipe = e=> {
-  //       console.log(update, 'updated recipe')
-  //       e.preventDefault();
-  //       console.log(update.id, 'match id')
-  //       props.updateRecipe(update.id ,update);
-  //       props.history.push('/ChefDashboard')
-  //     };
-//   return(
-//     <form onSubmit={updateRecipe}>
-//       <input value={recipe.recipe_name} name='recipe_name' id='recipename' onChange={handleChanges}/>
-//       <input value={recipe.meal_type} name='meal_type' id='mealtype' onChange={handleChanges}/>
-//       <button>Submit</button>
-//     </form>
-//   )     
-//  }
+  let id = location.pathname.substr(12, location.pathname.length - 1);
 
-//  export default EditRecipe;
+  const [recipe, updatedRecipe] = useState({
+    recipe_name: "",
+    chef_id: userID,
+    meal_type: "",
+  });
 
-//  const mapStateToProps = state => {
-//     return state;
-// }
-// export default connect(mapStateToProps, {updateRecipe})(EditRecipe);
+  const handleChanges = (event) => {
+    event.preventDefault();
+    updatedRecipe({ ...recipe, [event.target.name]: event.target.value });
+  };
 
-// const useStyles = makeStyles(theme => ({
-//     root: {
-//       '& .MuiTextField-root': {
-//         margin: theme.spacing(1),
-//         width: 250,
-//       },
-//     },
-//     paper:{
-  
-//     }
-  
-//   }));
+  const submitUpdatedRecipe = (event) => {
+    const updatedRecipe = {
+      recipe_name: recipe.recipe_name,
+      chef_id: userID,
+      meal_type: recipe.meal_type,
+    };
+    event.preventDefault();
+    props.updateRecipe(id, updatedRecipe);
+    props.history.push("/chefDashboard");
+  };
 
-//     const EditRecipe = (props) => {
+  return (
+    <div>
+      <form
+        onSubmit={submitUpdatedRecipe}
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+      >
+        <NavBar />
+        <h2>Edit Recipe</h2>
+        <Paper className={classes.paper}>
+          <div>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="age-native-simple">Meal Type</InputLabel>
+            </FormControl>
+          </div>
+          <br />
+          <div>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="recipename"
+              label="Recipe Name"
+              name="recipe_name"
+              autoFocus
+              value={recipe.recipe_name}
+              onChange={handleChanges}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="mealtype"
+              label="Meal Type"
+              name="meal_type"
+              autoFocus
+              value={recipe.meal_type}
+              onChange={handleChanges}
+            />
+          </div>
+          <div></div>
+          <br />
+        </Paper>
+        <div>
+          {" "}
+          <br />{" "}
+        </div>
 
-//     const classes = useStyles();
-//     const userID = localStorage.getItem('userID');
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          margin="normal"
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
 
-//     const [recipe, updatedRecipe] = useState({
-//         recipe_name: '',
-//         chef_id: userID,
-//         meal_type: ''
-//     });
+const mapStateToProps = (state) => {
+  return state;
+};
 
-//     useEffect(() => {
-//         props.updateRecipe(userID);
-//       }, []);
-//       console.log(props);
-
-//     const handleChanges = event => {
-//         event.preventDefault();
-//         updatedRecipe({ ...recipe, [event.target.name]: event.target.value });
-//       };
-    
-//       const submitUpdatedRecipe = event => {
-//         const updatedRecipe = {
-//           recipe_name: recipe.recipe_name,
-//           chef_id: recipe.userID,
-//           meal_type: recipe.meal_type
-//         };
-//         event.preventDefault();
-//         props.updateRecipe(updatedRecipe, userID);
-//         props.history.push('/chefDashboard')
-//       };
-
-    
-//     return (
-//         <div>
-//             <form onSubmit={submitUpdatedRecipe} className={classes.root} noValidate autoComplete="off">
-//     <NavBar/>
-//       <h2>EditRecipe Recipe</h2>
-//     <Paper className={classes.paper}>
-//     <div>
-//     <FormControl className={classes.formControl}>
-//     <InputLabel htmlFor="age-native-simple">Meal Type</InputLabel>    
-//   </FormControl> 
-//     </div>
-//     <br/>
-//     <div>
-//     <TextField
-//       variant="outlined"
-//       margin="normal"
-//       required
-//       fullWidth
-//       id="recipename"
-//       label="Recipe Name"
-//       name="recipe_name"
-//       autoFocus
-//       value={recipe.recipe_name}
-//       onChange={handleChanges}
-//     />
-//     <TextField
-//       variant="outlined"
-//       margin="normal"
-//       required
-//       fullWidth
-//       id="mealtype"
-//       label="Meal Type"
-//       name="meal_type"
-//       autoFocus
-//       value={recipe.meal_type}
-//       onChange={handleChanges}
-//     />
-//     </div>
-// <div>
-//       </div>
-//       <br/>
-
-//     </Paper>
-//       <div> <br/> </div>
-
-
-
-
-
-
-//       <Button type="submit" variant="contained" color="secondary" margin="normal">
-//         Submit
-//       </Button>
-//     </form>
-//         </div>
-//     )
-// }
-
-// const mapStateToProps = state => {
-//     return state;
-// }
-// export default connect(mapStateToProps, {updateRecipe})(EditRecipe);
-
-// import React, { useState } from "react";
-// import { connect } from 'react-redux';
-// import { updateRecipe } from '../../actions/RecipeActions/UpdateRecipe';
-// import NavBar from '../../components/NavBar.js';
-
-
-// const EditRecipe = (props) => {
-//     const userID = localStorage.getItem('userID');
-//     const recipe_id= props.recipe.id
-//     const classes = useStyles();
- 
-//     const [update, setUpdate] = useState({
-//       recipe_name: '',
-//       chef_id: userID,
-//       meal_type: ''
-//     });
-    
-//     const saveEdit= e=>{
-//         e.preventDefault()
-//         props.dispatch(updateRecipe(recipe_id))
-//     }
-
-
-//     const handleChanges = event => {
-//       setUpdate({ ...update, [event.target.name]: event.target.value });
-//     };
-  
-//     return (
-  
-  
-//       <form onSubmit={saveEdit} className={classes.root} noValidate autoComplete="off">
-//       <NavBar/>
-//         <h2>Add Recipe</h2>
-//       <Paper className={classes.paper}>
-//       <div>
-//       <FormControl className={classes.formControl}>
-//       <InputLabel htmlFor="age-native-simple">Meal Type</InputLabel>
-//        {/* <Select
-//         native
-  
-//         inputProps={{
-//           name: 'Meal Type',
-//           id: 'meal-type',
-//         }}
-//       >
-//         <option value="" />
-//         <option>Breakfast</option>
-//         <option>Lunch</option>
-//         <option>Dinner</option>
-//         <option>Snack</option>
-//         <option>Dessert</option>
-//       </Select> */}
-//     </FormControl> 
-  
-  
-//       </div>
-//       <br/>
-//       <div>
-  
-//       <TextField
-//         variant="outlined"
-//         margin="normal"
-//         required
-//         fullWidth
-//         id="recipename"
-//         label="Recipe Name"
-//         name="recipe_name"
-//         autoFocus
-//         value={update.recipe_name}
-//         onChange={handleChanges}
-//       />
-//       <TextField
-//         variant="outlined"
-//         margin="normal"
-//         required
-//         fullWidth
-//         id="mealtype"
-//         label="Meal Type"
-//         name="meal_type"
-//         autoFocus
-//         value={update.meal_type}
-//         onChange={handleChanges}
-//       />
-  
-  
-//       </div>
-//         {/* <div>
-//           <TextField
-//             id="outlined-multiline-static"
-//             label="Ingredients"
-//             multiline
-//             rows="20"
-//             defaultValue="Add Ingredients"
-//             variant="outlined"
-//           />
-//           <TextField
-//             id="outlined-multiline-static"
-//             label="Instructions"
-//             multiline
-//             rows="20"
-//             defaultValue="Add Steps"
-//             variant="outlined"
-//           />
-//         </div>
-//         <br/> */}
-  
-//   <div>
-//         {/* <Button  variant="contained" component="label">
-//           Upload Recipe Image
-//           <input
-//             type="file"
-//             style={{ display: "none"}}
-//           />
-//         </Button> */}
-//         </div>
-//         <br/>
-  
-//       </Paper>
-//         <div> <br/> </div>
-  
-  
-  
-  
-  
-  
-//         <Button type="submit" variant="contained" color="secondary" margin="normal">
-//           Submit
-//         </Button>
-//       </form>
-//     );
-//   }
-  
-//   const mapStateToProps = state => {
-//     return state;
-//   }
-  
-//   export default connect(mapStateToProps, { updateRecipe })(EditRecipe);
-
-
+export default connect(mapStateToProps, { updateRecipe })(EditRecipe);
--- src/components/Chef/RecipeCard.js
+++ src/components/Chef/RecipeCard.js
@@ -1,47 +1,45 @@
 /* jshint esversion: 6 */
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import { connect } from 'react-redux';
+import React from "react";
+import Card from "@material-ui/core/Card";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 //import components
-import {deleteRecipe} from '../../actions/RecipeActions/DeleteRecipe'
-import { makeStyles } from '@material-ui/core/styles';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import { deleteRecipe } from "../../actions/RecipeActions/DeleteRecipe";
+import { makeStyles } from "@material-ui/core/styles";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles(theme =>({
-    title: {
-      fontSize: 14,
-    },
-    flex: {
-      display: 'flex',
-      aliginItems: 'center',
-    },
-    chefAbout: {
-      display: 'flex',
-      aliginItems: 'center',
-
-
-    },
-    pos: {
-      marginBottom: 12,
-    },
-    media: {
-      height:150,
-      width: '50%',
-      marginLeft: '25%',
-      maxWidth: '100%',
-      maxHeight: 'auto',
-
- },
- root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
+const useStyles = makeStyles((theme) => ({
+  title: {
+    fontSize: 14,
+  },
+  flex: {
+    display: "flex",
+    aliginItems: "center",
+  },
+  chefAbout: {
+    display: "flex",
+    aliginItems: "center",
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  media: {
+    height: 150,
+    width: "50%",
+    marginLeft: "25%",
+    maxWidth: "100%",
+    maxHeight: "auto",
+  },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
@@ -49,101 +47,96 @@ const useStyles = makeStyles(theme =>({
     height: 450,
   },
   icon: {
-     marginRight: theme.spacing(2),
-   },
-   header: {
-     backgroundColor:'white',
-     padding: '60px',
-     display:'flex',
-     justifyContent:'center',
-
-   },
-   buttons: {
-     marginTop: theme.spacing(2),
-   },
-   cardGrid: {
-     paddingTop: theme.spacing(8),
-     paddingBottom: theme.spacing(8),
-   },
-   card: {
-     height: '100%',
-     display: 'flex',
-     flexDirection: 'column',
-     boxShadow: '3px 3px 2px gray'
-   },
-   cardMedia: {
-     paddingTop: '56.25%', // 16:9
-   },
-   cardContent: {
-     flexGrow: 1,
-   },
-   avatar: {
+    marginRight: theme.spacing(2),
+  },
+  header: {
+    backgroundColor: "white",
+    padding: "60px",
+    display: "flex",
+    justifyContent: "center",
+  },
+  buttons: {
+    marginTop: theme.spacing(2),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    boxShadow: "3px 3px 2px gray",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  avatar: {
     width: theme.spacing(18),
     height: theme.spacing(18),
-    marginLeft: '38%',
-   },
-   chefTitle: {
-     textDecoration: 'underline',
-     textDecorationColor: '#c62828'
-   },
-   cardBtns: {
-    backgroundColor: '#c62828',
-        '&:hover': {
-            backgroundColor: '#e53935'
-        }, 
-   },
-   btnCont: {
-    display: 'flex',
-    justifyContent: 'center'
-   }
+    marginLeft: "38%",
+  },
+  chefTitle: {
+    textDecoration: "underline",
+    textDecorationColor: "#c62828",
+  },
+  cardBtns: {
+    backgroundColor: "#c62828",
+    "&:hover": {
+      backgroundColor: "#e53935",
+    },
+  },
+  btnCont: {
+    display: "flex",
+    justifyContent: "center",
+  },
 }));
 
-const RecipeCard=(props)=> {
+const RecipeCard = (props) => {
   const classes = useStyles();
-  console.log('recipe card props', props)
+  console.log("recipe card props", props);
 
-    const recipe_id = props.recipe.id
-  
-    const submitDelete = event => {
-        event.preventDefault();
-        props.deleteRecipe(recipe_id);
-        document.location.reload('/ChefDashboard')
-      };
+  const recipe_id = props.recipe.id;
 
-    return (
-        <Grid item xs={12} sm={6} md={4}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.cardMedia}
-              image={'https://source.unsplash.com/featured/?Food'}
-              title="Image title"
-            />
-            <CardContent className={classes.cardContent}>
-              <Typography className={classes.chefTitle}>
-                Recipe Name
-              </Typography>
-              <Typography gutterBottom variant="h5" component="h2">
-                {props.recipe.recipe_name}
-              </Typography>
-              <Typography className={classes.chefTitle}>
-                Chef
-              </Typography>
-              <Typography>
-                {props.chefInfo.full_name}
-              </Typography>
-            </CardContent>
-            <CardActions className={classes.btnCont}>
-              {/* <Link to={`/editRecipe/${recipe_id}`}> */}
-                <Button className={classes.cardBtns}>Edit Recipe</Button>
-              {/* </Link> */}
-              <Button className={classes.cardBtns} onClick={submitDelete}>Remove Recipe</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-    );
+  const submitDelete = (event) => {
+    event.preventDefault();
+    props.deleteRecipe(recipe_id);
+    document.location.reload("/ChefDashboard");
+  };
+
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.cardMedia}
+          image={"https://source.unsplash.com/featured/?Food"}
+          title="Image title"
+        />
+        <CardContent className={classes.cardContent}>
+          <Typography className={classes.chefTitle}>Recipe Name</Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.recipe.recipe_name}
+          </Typography>
+          <Typography className={classes.chefTitle}>Chef</Typography>
+          <Typography>{props.chefInfo.full_name}</Typography>
+        </CardContent>
+        <CardActions className={classes.btnCont}>
+          <Link to={`/editRecipe/${recipe_id}`}>
+            <Button className={classes.cardBtns}>Edit Recipe</Button>
+          </Link>
+          <Button className={classes.cardBtns} onClick={submitDelete}>
+            Remove Recipe
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
 };
 
-const mapStateToProps = state => {
-    return state;
+const mapStateToProps = (state) => {
+  return state;
 };
 export default connect(mapStateToProps, { deleteRecipe })(RecipeCard);
